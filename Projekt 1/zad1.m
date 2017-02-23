31a7d5dc2dec62a13c7264d0336f99df65910428
mingless <mateusz.dziwulski@gmail.com>
2017-02-23 14:02:11 +0100

    Basic static and step response

--- Projekt 1/zad1.m
+++ Projekt 1/zad1.m
@@ -21,3 +21,19 @@ figure;
 plot(1:n, Y);
 figure;
 plot(1:n, U);
+
+close all;
+
+% Zad 2
+ 
+for i = 1:101
+    Us(i) = 1.5 - 0.5 + (i-1)*(1/100);
+    U(1:14) = 2.2;
+    U(15:n) = Us(i);
+    for k = 12:n
+        Y(k) = symulacja_obiektu5Y(U(k-10), U(k-11), Y(k-1), Y(k-2));
+    end
+    Ys(i) = Y(n);
+end
+figure;
+plot(Us, Ys);
