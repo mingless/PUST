b4de4ea287371fef9b182559a3b3dbb8cf64b792
Jakub Szczepański <xkubash@gmail.com>
2017-03-10 19:31:36 +0100

    Added noise to the DMC

--- Projekt 2/DMC.m
+++ Projekt 2/DMC.m
@@ -1,42 +1,33 @@
 clear all;
 
-n = 3000; %dlugosc symulacji, skróæ do 3000 w rysunkach do pdfa i wspomnij
-          %o tym w sprawku. wyd³u¿ona do usuniêcia oscylacji
-          %przy optymalizacji
-Yzad(1:n) = 2.2;  %rozne wartosci symulacji w roznych przedzialach
-Yzad(21:n) = 2.5; %mozliwe, ze trzeba je zmienic na relatywne od
-Yzad(1001:n)=2.4;  %punktu pracy, tj w przedziale y(umin)<yzad<y(umax)
-Yzad(1501:n)=1.85; %wtedy nalezy dodac w glownej petli odpowiednio
-Yzad(2201:n)=2.25; %odjac/dodac Ypp
+n = 200; 
+Yzad(1:20) = 0;
+Yzad(21:n) = 1;
+pomiar = 1;
 
-Ypp = 2.2; %punkt pracy
-Upp = 1.5;
-Y(1:n) = 2.2; %inicjalizacje tablic
-U(1:n) = 1.5;
-u = U - Upp;
+
+Y(1:n) = 0; %inicjalizacja tablic
+U(1:n) = 0;
+Z(1:n) = 0;
+%  Z(121:n) = 1;
 err = 0;
 
 %odpowiedzi skokowe pobrane z utworzonego wczesniej pliku
-s = fscanf(fopen('step_responses.txt', 'r'),'%f', [1 Inf]);
+s = fscanf(fopen('input_step_response.txt', 'r'),'%f', [1 Inf]);
+sz = fscanf(fopen('noise_step_response.txt', 'r'),'%f', [1 Inf]);
 fclose('all');
 
 
-D=120; 
-%parametry regulatora dobrane eksperymentalnie
-N=30; Nu=3; lambda=0.8; %err=15.6111
-%parametry dobrane skryptem param_optimizer
-%N=12; Nu=7; lambda=0.0103;
-%N=14; Nu=20; lambda=0.2290;
-%N=13; Nu=13; lambda=0;
-%N=37.000000; Nu=3.000000; lambda=0.015540; %final w/ err=sum(|e|) as quality index
-%N=64.000000; Nu=9.000000; lambda=0.013380; %final w/ normal err
-%N=33.000000; Nu=4.000000; lambda=0.018527; %really final, after changing DMC_err yzad
-                                %err = 15.5746
-%inicjalizacja macierzy dUp
+D=120; Dz=50;
+N=25; Nu=8; lambda=0.05; %err=5.9223
+
 for i=1:D-1
    dup(i)=0;
 end
 
+for i=1:Dz
+   dz(i)=0;
+end
 %generacja macierzy
 
 M=zeros(N,Nu);
@@ -59,61 +50,81 @@ for i=1:N
    end;
 end;
 
+Mz=zeros(N,Dz-1);
+for i=1:N
+   for j=1:Dz-1
+      if i+j<=Dz
+         Mz(i,j)=sz(i+j)-sz(j);
+      else
+         Mz(i,j)=sz(Dz)-sz(j);
+      end;      
+   end;
+end;
+Mz = [sz(1:N)' Mz];
 %przeksztalcanie wyliczonych macierzy do potrzebnych nam parametrow
 
 K=((M'*M+lambda*eye(Nu))^-1)*M';
 Ku=K(1,:)*Mp;
+Kz=K(1,:)*Mz;
 Ke=sum(K(1,:));
 
 %glowna petla
 
-for i=21:n
+for i=11:n
    
-   Y(i)=symulacja_obiektu5Y(U(i-10), U(i-11), Y(i-1), Y(i-2));
+   Y(i)=symulacja_obiektu2y(U(i-5), U(i-6), Z(i-3), Z(i-4), Y(i-1), Y(i-2));
    
    e=Yzad(i)-Y(i); %uchyb
    err = err + e^2;
    
    du=Ke*e-Ku*dup'; %regulator
-   
-   if du>0.1 %ograniczenia na szybkosc zmiany sygnalu sterowania
-       du = 0.1;
-   elseif du<-0.1
-       du = -0.1;
+   if pomiar
+       du = du - Kz*dz';
    end
+%    if du>0.1 %ograniczenia na szybkosc zmiany sygnalu sterowania
+%        du = 0.1;
+%    elseif du<-0.1
+%        du = -0.1;
+%    end
    
    for n=D-1:-1:2
       dup(n)=dup(n-1);
    end
-   dup(1)=du;
-   u(i)=u(i-1)+dup(1);
-   
-   if u(i)>0.5 %ograniczenia na min/max sygnalu sterowania
-       u(i) = 0.5;
-       dup(1) = u(i)-u(i-1);
-   elseif u(i)<-0.5
-       u(i) = -0.5;
-       dup(1) = u(i)-u(i-1);
+   for n=Dz:-1:2
+      dz(n)=dz(n-1);
    end
+   dup(1)=du;
+   dz(1)=Z(i)-Z(i-1);
+   U(i)=U(i-1)+dup(1);
+   
+%    if u(i)>0.5 %ograniczenia na min/max sygnalu sterowania
+%        u(i) = 0.5;
+%        dup(1) = u(i)-u(i-1);
+%    elseif u(i)<-0.5
+%        u(i) = -0.5;
+%        dup(1) = u(i)-u(i-1);
+%    end
    
    
-   U(i)=u(i)+Upp; %przesuniecie do punktu pracy
 end
 
 err
 
-figure('Position',  [403 246 820 420]);
-title('obiekt z regulatorem PID');
-subplot('Position', [0.1 0.12 0.8 0.15]);
+%wykresy
+figure('Position',  [403 146 560 525]);
+subplot('Position', [0.1 0.095 0.8 0.118]);
 stairs(U);
-ylabel('u'); 
+decimal_comma(gca, 'XY');
 xlabel('k');
+ylabel('u');
+subplot('Position', [0.1 0.295 0.8 0.118]);
+stairs(Z);
 decimal_comma(gca, 'XY');
-subplot('Position', [0.1 0.37 0.8 0.6]);
-plot(Y);
-ylabel('y'); 
+ylabel('z');
+subplot('Position', [0.1 0.495 0.8 0.48]);
+stairs(Y);
 decimal_comma(gca, 'XY');
-hold on; 
+ylabel('y');
+hold on;
 stairs(Yzad,':');
-
 
