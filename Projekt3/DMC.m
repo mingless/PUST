883a9ea8f96d897a86d40d6ddf4c893068be5b60
Jakub Szczepański <xkubash@gmail.com>
2017-04-02 14:38:28 +0200

    Final, working version of multidimensional DMC

--- Projekt3/DMC.m
+++ Projekt3/DMC.m
@@ -1,38 +1,33 @@
 clear all;
 
-%cos dziala, ale y1 sie dziwnie zachowuje
-%dziwne - odbicie S w poziomie nie wplywa na nic. Idk czy tak powinno byc
+%zdaje sie dzialac poprawnie
 
 %okropne indeksowania wynikacja z zawarcia wektorow w macierzach
 %prawdopodbnie mozna to uproscic korzystajac z cell, ale to komplikuje
-%obliczenia
+%i spowalnia obliczenia
 
 load('step_responses.mat');
-% S = zeros(1,1,190)
-% S(1,1,:) = fscanf(fopen('input_step_response.txt', 'r'),'%f', [1 Inf]);
-% fclose('all');
 
 nu = length(S(1,:,1));
 ny = length(S(:,1,1));
-% S = flip(S,1);
-% S = flip(S,2);
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %trajektorie zadane i parametry - do ustawienia
 
-n = 300; 
+n = 2000; %dlugosc symulacji
 
-D=100; 
+D=100;      %parametry regulatora
 N=100; Nu=100; lambda=1;
 
 Yzad(1:n*ny,1) = 0; %init
 
-%format Yzad: Yzad(NY+K*ny:n*ny) = YS
+%format wpisywania Yzad: Yzad(NY+K*ny:ny:n*ny) = YS
 %gdzie NY - numer wyjscia, K - czas skoku, YS - wartosc skoku
 
-Yzad(1+20*ny:ny:n*ny) = 1.4; %dla dwoch wyjsc - nieparzyste Yzad to
-Yzad(1+1000*ny:ny:n*ny)= 0.2;  %Y1zad, parzyste to Y2zad - przypisuje
-Yzad(1+1500*ny:ny:n*ny)= -0.8; % tutaj do konkretnych
+Yzad(1+20*ny:ny:n*ny) = 1.4; 
+Yzad(1+1000*ny:ny:n*ny)= 0.2;  
+Yzad(1+1500*ny:ny:n*ny)= -0.8; 
 
  Yzad(2+20*ny:ny:n*ny) = 1.4; 
  Yzad(2+150*ny:ny:n*ny)= 0.2;  
@@ -40,9 +35,9 @@ Yzad(1+1500*ny:ny:n*ny)= -0.8; % tutaj do konkretnych
 
  
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-Y(1:n*ny,1) = 0; %inicjalizacje tablic
+Y(1:n*ny,1) = 0; %inicjalizacje pionowych wektorow
 U(1:n*nu,1) = 0;
 err = 0;
 
@@ -59,7 +54,7 @@ for i=1:N
    for j=1:Nu
       if (i>=j)
          M(1+(i-1)*ny:i*ny, 1+(j-1)*nu:j*nu)=S(1:ny,1:nu,i-j+1);
-      end; %znajduje miejsce na odpowiednie S_n (bedace macierza) i je wpisuje
+      end; %znajduje miejsce na odpowiednie S_n (ny x nu) i je tam wpisuje
    end;
 end;
 
@@ -74,11 +69,16 @@ for i=1:N
    end;
 end;
 
-%przeksztalcanie wyliczonych macierzy do potrzebnych nam parametrow
+%przeksztalcanie wyliczonych macierzy do potrzebnych nam parametrow dla
+%zmniejszenia wymaganych obliczen
 
 K=((M'*M+lambda*eye(Nu*nu))^-1)*M';
 Ku=K(1:nu,:)*Mp; %pierwsze dwa rzedy K * Mp
-Ke(1:nu,1) = sum(K(1:nu,:),2); %oddzielnie zsumowane pierwsze nu wierszy macierzy K
+Ke = zeros(nu,ny);
+for j=1:ny %nu wierszy zawierajacych ny sum z K dla poszczegolnych wyjsc
+    Ke(:,j)=sum(K(1:nu,j:ny:end),2);
+end
+    
 
 %glowna petla
 
@@ -89,24 +89,20 @@ for i=21:n
    e=Yzad(1+(i-1)*ny:i*ny)-Y(1+(i-1)*ny:i*ny); %uchyb
    err = err + sum(e.^2);
    
-   du=Ke.*e-Ku*dup; %regulator
+   du=Ke*e-Ku*dup; %regulator
    
    
-   for ni=D-1:-1:2 %przesuniecie
+   for ni=D-1:-1:2 %przesuniecie dup
       dup(1+(ni-1)*nu:ni*nu)=dup(1+(ni-2)*nu:(ni-1)*nu);
       
    end
    dup(1:nu)=du;
    
-   U(1+(i-1)*nu:i*nu)=U(1+(i-2)*nu:(i-1)*nu)+dup(1:nu);
-   
-   
-   
-   
+   U(1+(i-1)*nu:i*nu)=U(1+(i-2)*nu:(i-1)*nu)+dup(1:nu); %wyznaczenie nowego
+                                                        %sterowania
 end
 
 err
-
 
 figure('Position',  [403 0 620 725]);
 subplot('Position', [0.1 0.069 0.8 0.0855]); %62 at 50
