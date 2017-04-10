fa976efda4b8e894bc01329a420c268d2f8b276d
mingless <mateusz.dziwulski@gmail.com>
2017-04-10 09:54:24 +0200

    Adjusted P3/PID for lab use.

--- Lab3/PID.m
+++ Lab3/PID.m
@@ -1,71 +1,114 @@
-clear all
+close all;
+clear all;
+
 addpath('F:\SerialCommunication'); % add a path to the functions
 initSerialControl COM4 % initialise com port
- 
-Ypp=32.31;
-Upp=30;
-n = 640;
-% Yzad(1:n) = 1.9;
-% Yzad(21:n) = 1.6;
-% Yzad(1001:n)=1.4;
-% Yzad(1501:n)=2.5;
-% Yzad(2201:n)=1.4;
-Yzad(1:40)=Ypp;
-Yzad(41:340)=40;
-Yzad(341:n)=35;
 
-Y(1:n) = Ypp;
-u(1:n) = Upp;
+n = 1000;
+
+Yzad(1:2,1:n)=0;
+Yzad(1,21:end)=1.4;
+Yzad(1,601:end)=0.2;
+Yzad(1,1001:end)=-0.8;
+Yzad(2,151:end)=-0.3;
+Yzad(2,401:end)=0.9;
+Yzad(2,801:end)=-0.7;
+
+U(1:2,1:n)=0;
+Y(1:2,1:n)=0;
+
 err = 0;
- 
-%values from optimization
-K=7.19; Ti=102.8612; Td=0; Ts=1;%
- 
-r2 = K*Td/Ts;
-r1 = K*(Ts/(2*Ti)-2*Td/Ts - 1);
-r0 = K*(1+Ts/(2*Ti) + Td/Ts);
- 
-for k=21:n
-    measurements = readMeasurements(1:7);
-     Y(k)=measurements(1);
-     
-     e(k)=Yzad(k)-Y(k);
-      %% processing of the measurements and new control values calculation
-        
-     du = r2*e(k-2)+r1*e(k-1)+r0*e(k);
-     
-     u(k)=du+u(k-1);
-     
-     U(k)= u(k) + Upp;
-     
-     if U(k)>100
-         U(k) = 100;
+
+%przy reg1: oscylacje krytyczne przy K1=2.2, Tk1=20;
+%przy reg2: oscylacje krytyczne przy K2=6.9, Tk2=10;
+%po zamianie we/wy
+%przy reg1: oscylacje krytyczne przy K1=4.6, Tk1=5;
+%przy reg2: oscylacje krytyczne przy K2=11.9, Tk2=5;
+%Z-N nie dzialal (?)
+
+%Eksperymentalnie
+
+
+% K(1)=1.1; Ti(1)=9; Td(1)=0.8; Ts(1)=0.5;
+% K(1)=0; Ti(1)=inf; Td(1)=0; Ts(1)=0.5;
+% K(2)=3.45; Ti(2)=9; Td(2)=0.3; Ts(2)=0.5;
+% K(2)=0; Ti(2)=inf; Td(2)=0; Ts(2)=0.5;
+% K(1)=0; Ti(1)=inf; Td(1)=0; Ts(1)=0.5;
+% K(2)=4.45; Ti(2)=6; Td(2)=0.1; Ts(2)=0.5;
+% K(1)=0.3; Ti(1)=1; Td(1)=0.5; Ts(1)=0.5;
+
+% Parametry z Lab1: K=7.19; Ti=102.8612; Td=0; Ts=1;%
+%parametry dobrane z fmincon(@PID_err,[1.1 9 0.8 3.45 9 0.3],[],[],[],[],[0 0 0 0 0 0],[])
+K(1)=1.1576; Ti(1)=12.5191; Td(1)=1.4089; Ts(1)=0.5;
+K(2)=0.8789; Ti(2)=7.9788; Td(2)=0.5366; Ts(2)=0.5;
+e(1:2,1:n)=0;
+
+K=K'; Ti=Ti'; Td=Td'; Ts=Ts';
+
+
+r2 = K.*Td./Ts; r1 = K.*(Ts./(2.*Ti)-2.*Td./Ts - 1); r0 = K.*(1+Ts./(2.*Ti) + Td./Ts);
+
+
+%glowna petla PID
+for k=3:n
+     measurements = readMeasurements(1:7);
+     Y(1,k)=measurements(1);
+     Y(2,k)=measurements(1);
+
+     e(:,k)=Yzad(:,k)-Y(:,k); %blad wyjscia
+
+     du = r2.*e(:,k-2)+r1.*e(:,k-1)+r0.*e(:,k);
+
+     %du = du([2 1]); %obrocenie konfiguracji regulatora
+
+     U(:,k)=du(:)+U(:,k-1);
+
+     if U(1,k)>100
+         U(1,k) = 100;
      end
-     if U(k)<0
-         U(k) = 0;
+     if U(1,k)<0
+         U(1,k) = 0;
      end
-     
+     if U(2,k)>100
+         U(2,k) = 100;
+     end
+     if U(2,k)<0
+         U(2,k) = 0;
+     end
      %% sending new values of control signals
-     disp([measurements(1), Upp, u(k), measurements(5), k, U(k), Y(k)]); % process measurements
+     disp([measurements(1), U(:,k), Y(:,k), Yzad(:,k)]); % process measurements
 
      sendControls([ 1, 2, 3, 4, 5, 6], ... send for these elements
-                     [50, 0, 0, 0, U(k), 0]);  % new corresponding control values
-     
+                     [50, 0, 0, 0, U(1,k), U(2,k)]);  % new corresponding control values
      %% synchronising with the control process
      waitForNewIteration(); % wait for new batch of measurements to be ready
-     
 end;
- 
-err = sum(e.^2)
- 
-figure('Position',  [403 246 820 420]);
-subplot('Position', [0.1 0.12 0.8 0.15]);
-stairs(U);
-ylabel('u');
-xlabel('k');
-subplot('Position', [0.1 0.37 0.8 0.6]);
 
-stairs(Y);
-ylabel('y');
-hold on;
-stairs(Yzad,':');
+err = trace(e'*e) %blad funkcji
+
+    if exist('YoldPID.mat','file')
+        load('YoldPID.mat');
+    end
+    figure('Position',  [403 0 620 725]);
+    subplot('Position', [0.1 0.069 0.8 0.0855]); %62 at 50
+    stairs(U(2,:));
+    decimal_comma(gca, 'XY');
+    xlabel('k');
+    ylabel('u_2');
+    subplot('Position', [0.1 0.2138 0.8 0.0855]); %62 at 155
+    stairs(U(1,:));
+    % ylim([-5 5]);
+    decimal_comma(gca, 'XY');
+    ylabel('u_1');
+    subplot('Position', [0.1 0.3586 0.8 0.2759]); %200 at 260
+    ff = plot(Y(2,:));
+    hold on;
+    plot(Yzad(2,:),':');
+    decimal_comma(gca, 'XY');
+    ylabel('y_2');
+    subplot('Position', [0.1 0.6897 0.8 0.2759]); %200 at 500
+    ff = plot(Y(1,:));
+    hold on;
+    plot(Yzad(1,:),':');
+    decimal_comma(gca, 'XY');
+    ylabel('y_1');
